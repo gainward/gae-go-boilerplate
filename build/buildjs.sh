5259ef50b1a47819838b47f5bca00f2f60b2c2a6
Wes <readybreak@gmail.com>
2012-04-06 12:56:10 -0700

    Removing closure-library and closure-templates from build dir, so removing them from gitignore.  Removing unused buildjs and buildtemplates shell scripts.

--- build/buildjs.sh
+++
@@ -1,7 +0,0 @@
-closure-library/closure/bin/build/closurebuilder.py \
-	--root=closure-library/ \
-	--root=../js/ \
-	--namespace=${JS_NAMESPACE} \
-	--output_mode="script" \
-	closure-templates/soyutils_usegoog.js \
-	> ${JS_OUTPUT_FILE}
--- build/buildtemplates.sh
+++
@@ -1,5 +0,0 @@
-java -jar closure-templates/SoyToJsSrcCompiler.jar \
-  --shouldProvideRequireSoyNamespaces \
-  --shouldGenerateJsdoc \
-  --outputPathFormat ../js/templates/{INPUT_FILE_NAME_NO_EXT}.js \
-  ../templates/sandbar.soy ../templates/mainview.soy
